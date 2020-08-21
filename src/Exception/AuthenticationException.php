3e54cff3b2a7c0fb5200468a99fbde35b169e552
Casper Bakker <casper@picqer.com>
2020-08-21 09:53:59 +0200

    Fix authentication exceptions

--- src/Exception/AuthenticationException.php
+++ src/Exception/AuthenticationException.php
@@ -8,7 +8,7 @@ class AuthenticationException extends \Exception
 {
     public function __construct(?string $message = null, int $code = 0, Exception $previous = null)
     {
-        $message = $message ?: 'An unknown error occured during the authentication with Bol.com';
+        $message = $message ?: 'An unknown error occurred during the authentication with Bol.com';
 
         parent::__construct($message, $code, $previous);
     }
