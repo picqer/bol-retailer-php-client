ebd3207cc19b572adfa3393bdaeb0b4c86a9ee28
Rob van Aarle <robvanaarle@gmail.com>
2021-02-14 16:57:29 +0100

    Added tests for main request method of BaseClient

--- testsv4/BaseClientTest.php
+++ testsv4/BaseClientTest.php
@@ -11,6 +11,8 @@ use PHPUnit\Framework\TestCase;
 use Picqer\BolRetailerV4\BaseClient;
 use Picqer\BolRetailerV4\Exception\ResponseException;
 use Picqer\BolRetailerV4\Exception\UnauthorizedException;
+use Picqer\BolRetailerV4\Model\AbstractModel;
+use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Prophecy\Prophecy\ObjectProphecy;
 
@@ -22,13 +24,28 @@ class BaseClientTest extends TestCase
     private $client;
 
     /** @var ObjectProphecy */
-    private $http;
+    private $httpProphecy;
+
+    /** @var ObjectProphecy */
+    private $modelClass;
 
     public function setup(): void
     {
-        $this->http = $this->prophesize(ClientInterface::class);
+        $this->httpProphecy = $this->prophesize(ClientInterface::class);
         $this->client = new BaseClient();
-        $this->client->setHttp($this->http->reveal());
+        $this->client->setHttp($this->httpProphecy->reveal());
+
+        $stub = new class () extends AbstractModel {
+            public $foo;
+
+            public function getModelDefinition(): array
+            {
+                return [
+                    'foo' => [ 'model' => null, 'array' => false ]
+                ];
+            }
+        };
+        $this->modelClass = get_class($stub);
     }
 
     public function testClientIsInitiallyNotAuthenticated()
@@ -36,12 +53,12 @@ class BaseClientTest extends TestCase
         $this->assertFalse($this->client->isAuthenticated());
     }
 
-    public function testClientIsAuthenticatedAfterSuccessfulAuthentication()
+    protected function authenticate()
     {
         $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-token'));
 
         $credentials = base64_encode('secret_id' . ':' . 'somesupersecretvaluethatshouldnotbeshared');
-        $this->http->request('POST', 'https://login.bol.com/token', [
+        $this->httpProphecy->request('POST', 'https://login.bol.com/token', [
             'headers' => [
                 'Accept' => 'application/json',
                 'Authorization' => 'Basic ' . $credentials
@@ -52,6 +69,11 @@ class BaseClientTest extends TestCase
         ])->willReturn($response);
 
         $this->client->authenticate('secret_id', 'somesupersecretvaluethatshouldnotbeshared');
+    }
+
+    public function testClientIsAuthenticatedAfterSuccessfulAuthentication()
+    {
+        $this->authenticate();
 
         $this->assertTrue($this->client->isAuthenticated());
     }
@@ -66,7 +88,7 @@ class BaseClientTest extends TestCase
         );
 
         $credentials = base64_encode('secret_id' . ':' . 'somesupersecretvaluethatshouldnotbeshared');
-        $this->http->request('POST', 'https://login.bol.com/token', [
+        $this->httpProphecy->request('POST', 'https://login.bol.com/token', [
             'headers' => [
                 'Accept' => 'application/json',
                 'Authorization' => 'Basic ' . $credentials
@@ -105,7 +127,7 @@ class BaseClientTest extends TestCase
     public function testAuthenticateThrowsResponseExceptionWhenTokenIsMalformed($response)
     {
         $credentials = base64_encode('secret_id' . ':' . 'somesupersecretvaluethatshouldnotbeshared');
-        $this->http->request('POST', 'https://login.bol.com/token', [
+        $this->httpProphecy->request('POST', 'https://login.bol.com/token', [
             'headers' => [
                 'Accept' => 'application/json',
                 'Authorization' => 'Basic ' . $credentials
@@ -117,5 +139,118 @@ class BaseClientTest extends TestCase
 
         $this->expectException(ResponseException::class);
         $this->client->authenticate('secret_id', 'somesupersecretvaluethatshouldnotbeshared');
+    }
+
+    public function testRequestReturnsModel()
+    {
+        $this->authenticate();
+
+        $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-foo'));
+        $this->httpProphecy->request(Argument::cetera())->willReturn($response);
+
+        $response = $this->client->request('GET', 'foobar', [], [
+            '200' => $this->modelClass
+        ]);
+
+        $this->assertInstanceOf($this->modelClass, $response);
+        $this->assertEquals('bar', $response->foo);
+    }
+
+    public function testRequestReturnsString()
+    {
+        $this->authenticate();
+
+        $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-string'));
+        $this->httpProphecy->request(Argument::cetera())->willReturn($response);
+
+        $response = $this->client->request('GET', 'foobar', [], [
+            '200' => 'string'
+        ]);
+
+        $this->assertEquals("This is a test string\n", $response);
+    }
+
+    public function testRequestThrowsResponseExceptionAtUnknownResponseType()
+    {
+        $this->authenticate();
+
+        $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-string'));
+        $this->httpProphecy->request(Argument::cetera())->willReturn($response);
+
+        $this->expectException(ResponseException::class);
+        $this->client->request('GET', 'foobar', [], []);
+    }
+
+    public function testRequestAddsProducesAsAcceptHeader()
+    {
+        $this->authenticate();
+
+        $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-string'));
+        $this->httpProphecy
+            ->request('GET', 'https://api.bol.com/retailer/foobar', [
+                'headers' => [
+                    'Accept' => 'application/vnd.retailer.v4+pdf',
+                    'Authorization' => 'Bearer ' . $this->client->getToken()['access_token']
+                ]
+            ])
+            ->willReturn($response)
+            ->shouldBeCalled();
+
+        $this->client->request('GET', 'foobar', [
+            'produces' => 'application/vnd.retailer.v4+pdf'
+        ], [
+            '200' => 'string'
+        ]);
+    }
+
+    public function testRequestJsonEncodesBodyModelIntoBody()
+    {
+        $this->authenticate();
+
+        $model = new $this->modelClass();
+        $model->foo = 'bar';
+
+        $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-string'));
+        $this->httpProphecy
+            ->request('GET', 'https://api.bol.com/retailer/foobar', [
+                'headers' => [
+                    'Accept' => 'application/vnd.retailer.v4+json',
+                    'Authorization' => 'Bearer ' . $this->client->getToken()['access_token'],
+                    'Content-Type' => 'application/vnd.retailer.v4+json'
+                ],
+                'body' => json_encode($model)
+            ])
+            ->willReturn($response)
+            ->shouldBeCalled();
+
+        $this->client->request('GET', 'foobar', [
+            'body' => $model,
+        ], [
+            '200' => 'string'
+        ]);
+    }
+
+    public function testRequestConstructsEndpoint()
+    {
+        $this->authenticate();
+
+        $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-string'));
+        $this->httpProphecy
+            ->request('GET', 'https://api.bol.com/retailer/foobar', Argument::cetera())
+            ->willReturn($response)
+            ->shouldBeCalled();
+
+        $this->client->request('GET', 'foobar', [], [
+            '200' => 'string'
+        ]);
+    }
+
+    public function testRequestThrowsUnauthorizedExceptionWhenNotAuthenticated()
+    {
+        $response = Message::parseResponse(file_get_contents(__DIR__ . '/Fixtures/http/200-foo'));
+        $this->httpProphecy->request(Argument::cetera())->willReturn($response);
+
+        $this->expectException(UnauthorizedException::class);
+        $this->client->request('GET', 'foobar', [], []);
     }
 }
