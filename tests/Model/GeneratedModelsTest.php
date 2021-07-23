0c05e0f77e6b42c55c7ccf15b6635289815bdbff
Rob van Aarle <rob@picqer.com>
2021-07-23 06:51:35 +0200

    Added test to instantiate all generated models

---
+++ tests/Model/GeneratedModelsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Picqer\BolRetailer\Tests\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class GeneratedModelsTest extends TestCase
+{
+    public function provideGeneratedModelClassnames()
+    {
+        $fileNames = scandir("src/Model");
+
+        $fileNames = array_filter($fileNames, function ($fileName) {
+            return ! in_array($fileName, ['.', '..', 'AbstractModel.php']);
+        });
+
+        return array_map(function ($fileName) {
+            return ['Picqer\\BolRetailer\\Model\\' . substr($fileName, 0, -4)];
+        }, $fileNames);
+    }
+
+    /**
+     * @dataProvider provideGeneratedModelClassnames
+     */
+    public function testGeneratedModelCanBeInstantiated(string $modelClassname)
+    {
+        $instance = new $modelClassname();
+        $this->assertEquals(get_class($instance), $modelClassname);
+    }
+}
